3a5f4bb36e424c6fdb091f532ac20b7007286f53
Maksimilian <m.ceresniovyj@locatory.com>
2021-03-17 17:22:11 +0200

    dependencies & added redux store

--- btc/src/index.tsx
+++ btc/src/index.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {configureStore} from "./store";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Application: React.FC = () => (
+   <Provider store={configureStore()}>
+       <App />
+   </Provider>
+)
+
+ReactDOM.render(<Application />,
   document.getElementById('root')
 );
 
