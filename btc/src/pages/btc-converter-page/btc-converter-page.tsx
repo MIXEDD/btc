08623c66331127992f3849ca346d03e897710add
Maksimilian <m.ceresniovyj@locatory.com>
2021-03-20 13:23:54 +0200

    interval

--- btc/src/pages/btc-converter-page/btc-converter-page.tsx
+++ btc/src/pages/btc-converter-page/btc-converter-page.tsx
@@ -6,16 +6,25 @@ import { CoindeskBtcModel } from '../../types/BtcConverter';
 
 import styles from './btc-converter-page.module.scss';
 
+const SECOND_IN_MS = 60000;
+
 const BtcConverterPage: React.FC = () => {
     const [data, setData] = React.useState<CoindeskBtcModel[]>();
-    
-    React.useEffect(() => {
+
+    const fetchBtcValues = React.useCallback(() => {
         btcConvertService.fetchBtcValues().then((res) =>
             setData(Object.keys(res.data.bpi).map((bpi) => res.data.bpi[bpi]) as CoindeskBtcModel[] )
         );
     }, []);
+
+    React.useEffect(() => {
+        fetchBtcValues();
+
+        setInterval(() => {
+            fetchBtcValues();
+        }, SECOND_IN_MS);
+    }, []);
     
-   
    return (
        <div className={styles.container}>
           <BtcConverterForm />
